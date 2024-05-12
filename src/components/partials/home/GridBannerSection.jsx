c1959aa4ff5f49cdd615ed74bdd6ae0592519be9
dthemesdevelopment <dthemesdevelopment@gmail.com>
2024-05-12 21:24:39 +0300

    updating ui

--- src/components/partials/home/GridBannerSection.jsx
+++ src/components/partials/home/GridBannerSection.jsx
@@ -6,10 +6,9 @@ const GridBannerSection = () => {
     {
       title:
         "Welcome to the future of automotive customization and performance enhancement.",
-      desc: `Welcome to the future of automotive customization and performance enhancement.
-      Today, we're thrilled to introduce German Auto Parts the epitome of excellence in the world of premium spare parts and tuning.
-      At Audi, we represent the pinnacle of automotive engineering, offering a curated selection of products for Audi, BMW, AMG, Mercedes, Seat, Skoda, Porsche, and VW enthusiasts.
-      With a commitment to quality, performance, and innovation, we're here to elevate your driving experience to unprecedented heights.`,
+      desc: `Welcome to the future of automotive customization and performance enhancement. Today, we're thrilled to introduce German Auto Parts the epitome of excellence in the world of premium spare parts and tuning.
+
+      We represent the pinnacle of automotive engineering, offering a curated selection of products for Audi, BMW, AMG, Mercedes, Seat, Skoda, Porsche, and VW enthusiasts. With a commitment to quality, performance, and innovation, we're here to elevate your driving experience to unprecedented heights.`,
       imgUrl: "/images/banners/banner-2.jpg",
       button: {
         title: "Design & Order",
@@ -29,9 +28,10 @@ const GridBannerSection = () => {
     },
     {
       title: "Q6 e-tron",
-      desc: `Dive into our extensive catalog featuring an array of premium spare parts and tuning options for Audi, BMW, AMG, Mercedes, Seat, Skoda, Porsche, and VW models.
-      From performance exhaust systems and suspension upgrades to bespoke carbon fiber components and cutting-edge ECU tuning, we have everything you need to enhance both the aesthetics and performance of your vehicle.
-      Our partnerships with the most prestigious brands in the industry ensure that only the finest products grace our shelves, guaranteeing excellence in every purchase.`,
+      desc: `Dive into our extensive catalog featuring an array of premium spare parts and tuning options for Audi, BMW, AMG, Mercedes, Seat, Skoda, Porsche, and VW models. From performance exhaust systems and suspension upgrades to bespoke carbon fiber components and cutting-edge ECU tuning, we have everything you need to enhance both the aesthetics and performance of your vehicle.
+
+      Our partnerships with the most prestigious brands in the industry ensure that only the finest products grace our shelves, guaranteeing excellence in every purchase.
+      `,
       imgUrl: "/images/banners/banner-4.jpg",
       button: {
         title: "Design & Order",
@@ -40,16 +40,12 @@ const GridBannerSection = () => {
     },
     {
       title: "Q6 e-tron",
-      desc: `Experience convenience like never before with our state-of-the-art webshop platform.
-      Our user-friendly interface and intuitive navigation make finding the perfect products for your vehicle effortless.
-      Enjoy secure transactions, fast shipping, and dedicated customer support every step of the way, ensuring a hassle-free shopping experience from start to finish.
+      desc: `Experience convenience like never before with our state-of-the-art webshop platform. Our user-friendly interface and intuitive navigation make finding the perfect products for your vehicle effortless. Enjoy secure transactions, fast shipping, and dedicated customer support every step of the way, ensuring a hassle-free shopping experience from start to finish.
+
+      Proudly serving automotive enthusiasts across all EU countries, we bring our passion for premium spare parts and tuning to enthusiasts far and wide. With streamlined logistics and efficient distribution channels, we ensure timely delivery to your doorstep, no matter where you are in Europe (EU). Join our growing community of enthusiasts and embark on a journey of automotive excellence with German Auto Parts.
       
-      
-      Proudly serving automotive enthusiasts across all EU countries, we bring our passion for premium spare parts and tuning to enthusiasts far and wide.
-      With streamlined logistics and efficient distribution channels, we ensure timely delivery to your doorstep, no matter where you are in Europe (EU). Join our growing community of enthusiasts and embark on a journey of automotive excellence with German Auto Parts.
-      
-      Thank you for joining us on this journey as we revolutionize the automotive aftermarket industry.
-      Whether you're seeking to enhance the performance, style, or individuality of your vehicle, [Your Company Name] is here to turn your dreams into reality. Explore our webshop today and discover the endless possibilities that await you.`,
+      Thank you for joining us on this journey as we revolutionize the automotive aftermarket industry. Whether you're seeking to enhance the performance, style, or individuality of your vehicle, German Auto Parts is here to turn your dreams into reality.
+      Explore our webshop today and discover the endless possibilities that await you.`,
       imgUrl: "/images/banners/banner-5.jpg",
       button: {
         title: "Design & Order",
@@ -80,9 +76,11 @@ const GridBannerSection = () => {
                 </div>
               </div>
 
-              <div className={`col-span-12 md:col-span-6 md:px-20 py-5 md:py-10`}>
+              <div
+                className={`col-span-12 md:col-span-6 md:px-20 py-5 md:py-10`}
+              >
                 {/* <h3 className="text-3xl mb-10">{item.title}</h3> */}
-                <p className="lg:text-xl mb-10">{item.desc}</p>
+                <p className="lg:text-xl">{item.desc}</p>
                 {/* <ALink
                   href={"/"}
                   className="flex items-center justify-start space-x-2"
